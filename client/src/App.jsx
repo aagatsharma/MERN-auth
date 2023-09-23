df199105d0739483036d6fe380e57e595a6513ef
Aagat Sharma <aagats72@gmail.com>
2023-09-23 19:38:34 +0545

    added navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,50 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import About from "./pages/About";
+
+const Layout = () => (
+  <>
+    <Header />
+    <main>
+      <Outlet />
+    </main>
+  </>
+);
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/profile",
-    element: <Profile />,
+    element: <Layout />,
+    errorElement: <h1>404 Not found</h1>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
   },
 ]);
 
 export default function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
