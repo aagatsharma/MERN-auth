f06e8f2ea1217ab0c5970d0151767b82ea490d56
Aagat Sharma <aagats72@gmail.com>
2023-09-23 18:33:27 +0545

    added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
+
 export default function App() {
   return (
-    <div>
-      <h1 className="bg-red-500">Hello</h1>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
