249c28c2bad344a7823279fcb1ce3db34f72151d
Aagat Sharma <aagats72@gmail.com>
2023-09-24 23:13:00 +0545

    test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 import userRoute from "./routes/userRoute.js";
+import authRoute from "./routes/authRoute.js";
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("Database connected successfully"))
@@ -14,3 +16,4 @@ app.listen(3000, () => {
 });
 
 app.use("/api", userRoute);
+app.use("/api/auth", authRoute);
