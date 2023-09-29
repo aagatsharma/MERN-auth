6adc13611e959e10b320becc2c99776240a76a8c
aagatsharma <aagats72@gmail.com>
2023-09-29 14:45:35 +0545

    image upload

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -11,7 +11,6 @@ export default function OAuth() {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
-      console.log(result);
       const res = await fetch(googleRoute, {
         method: "POST",
         headers: {
